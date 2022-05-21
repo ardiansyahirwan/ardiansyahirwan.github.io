cfc8c2c16433fb918f2bf5cb4328e0b583541cff
ardiansyahirwan <ardiansyahirwan923@gmail.com>
2022-05-22 04:27:57 +0700

    add navigation with changes the element

--- public/js/main.js
+++ public/js/main.js
@@ -7,11 +7,22 @@ function pageTransition(){
    for(let i = 0 ; i < control.length; i++ ){
       control[i].addEventListener('click',function(){
          let currButton = document.querySelectorAll('.active-btn')
-         console.log(currButton)
          currButton[0].className = currButton[0].className.replace('active-btn','')
          this.classList.add('active-btn')
       })
    }
+
+   bodyElement.addEventListener('click', function(e){
+      const id = e.target.dataset.id;
+      if(id){
+         section.forEach(element => {
+            element.classList.remove('active')
+         });
+         const element = document.getElementById(id)
+         element.classList.add('active')
+         
+      }
+   })
 }
 
 pageTransition()
