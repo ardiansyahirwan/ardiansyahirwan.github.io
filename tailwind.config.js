4aa0ab90b75df3a9b83d4daa9e4dbc07f450ab95
ardiansyahirwan <ardiansyahirwan923@gmail.com>
2022-05-24 23:24:56 +0700

    index.html : change background color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes:{
+        scale:{
+          '0%':{transform:'translateY(-100%) scaleY(0)'},
+          '100%':{transform:'translateY(0) scaleY(1)'},
+        }
+      },
+      animation:{
+        'scale-anim':'scale 1s ease-in-out',
+      },
+      colors:{
+        neon:{
+          blue:'#16AAFC',
+          pink:'#A709D0',
+        },
+      }
+    },
   },
   plugins: [],
 }
